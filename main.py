84873d91155f9437bf938d7bedf2e183d3a7c2fe
LuchoAGN <44987778+LuchoAGN@users.noreply.github.com>
2024-08-18 17:29:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,32 @@ from app.database import engine, Base
 # Crea todas las tablas
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+
+description = """
+Un backend para una aplicación de gestión de tareas (Todo) desarrollado con FastAPI y SQLAlchemy, utilizando MySQL para 
+la base de datos. Esta API proporciona funcionalidades de autenticación, manejo de tareas y gestión de usuarios. 🚀
+
+Este proyecto es un backend para una aplicación de gestión de tareas. Proporciona endpoints para:
+
+- Registro y autenticación de usuarios
+- Gestión de tareas (crear, leer, actualizar y eliminar)
+- Recuperación de información del usuario actual
+"""
+
+app = FastAPI( title="Service ToDo",
+    description=description,
+    summary="My favorite basic service",
+    version="0.0.1",
+    terms_of_service="https://github.com/LuchoAGN/service_todo?tab=MIT-1-ov-file",
+    contact={
+        "name": "Lucho AGN",
+        "url": "https://luchoagn.dev/",
+        "email": "contact@luchoagn.dev",
+    },
+    license_info={
+        "name": "MIT license",
+        "url": "https://opensource.org/license/mit",
+    },)
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(todo.router, prefix="/todos", tags=["todos"])
